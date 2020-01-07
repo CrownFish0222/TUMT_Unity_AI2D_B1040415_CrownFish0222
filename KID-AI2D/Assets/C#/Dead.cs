6360be73ba8d90b1c60363043d97976c8d927ea1
CrownFish0222 <55520857+CrownFish0222@users.noreply.github.com>
2020-01-08 03:37:52 +0800

    機密

---
+++ KID-AI2D/Assets/C#/Dead.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;           //引用 事件 API
+using UnityEngine.UI;               //引用 事件 API
+
+public class Dead : MonoBehaviour
+{
+    public GameObject final;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "QQ")
+        {
+           final.SetActive(true);
+        }
+         
+    }
+}
